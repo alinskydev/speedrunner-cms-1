a45d93c0bc8b5a44f88358e50295d09f67e35678
Dmitry Alinsky <alinsky.dmitry@gmail.com>
2021-10-22 17:02:48 +0500

    Minor update

---
+++ common/config/main-local.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=sr-cms-1',
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8mb4',
+        ],
+    ],
+];

1726cafb6fea340f53b6873aa47a70ce58be0897
Dmitry Alinsky <alinsky.dmitry@gmail.com>
2021-12-27 19:36:49 +0500

    Update ImageHelper.php

--- speedrunner/helpers/ImageHelper.php
+++ speedrunner/helpers/ImageHelper.php
@@ -3,6 +3,7 @@
 namespace speedrunner\helpers;
 
 use Yii;
+use yii\helpers\Html;
 use yii\helpers\ArrayHelper;
 use yii\helpers\StringHelper;
 use yii\helpers\FileHelper;
@@ -51,5 +52,55 @@ class ImageHelper
         } else {
             return $image_url;
         }
+    }
+    
+    public static function sourceTag($image_url, $width_height = null, $thumb_type = 'resize', $options = [])
+    {
+        if ($width_height) {
+            $image_url = self::thumb($image_url, $width_height, $thumb_type);
+        }
+        
+        $image = Yii::getAlias('@frontend/web') . $image_url;
+        
+        if (!is_file($image)) {
+            return false;
+        }
+        
+        $image_mime_type = mime_content_type($image);
+        
+        if (in_array($image_mime_type, ['image/png', 'image/jpeg'])) {
+            $dir = Yii::getAlias('@frontend/web/assets/thumbs/webp');
+            FileHelper::createDirectory($dir);
+            
+            $image_name = md5(filemtime($image) . filesize($image)) . '.webp';
+            $source_image = "$dir/$image_name";
+            
+            if (!is_file($source_image)) {
+                switch ($image_mime_type) {
+                    case 'image/png':
+                        $image = imagecreatefrompng($image);
+		        	    imagepalettetotruecolor($image);
+		        	    imagealphablending($image, true);
+		        	    imagesavealpha($image, true);
+                        break;
+                        
+                    case 'image/jpeg':
+                        $image = imagecreatefromjpeg($image);
+		        	    imagepalettetotruecolor($image);
+                        break;
+                }
+                
+                imagewebp($image, $source_image);
+            }
+            
+            $source_image = str_replace(Yii::getAlias('@frontend/web'), null, $source_image);
+        } else {
+            $source_image = $image_url;
+        }
+        
+        return Html::tag('source', null, ArrayHelper::merge([
+            'srcset' => $source_image,
+            'type' => mime_content_type(Yii::getAlias("@frontend/web/$source_image")),
+        ], $options));
     }
 }

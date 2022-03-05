3904801dcf16fc7dc732519ab54f1ae31d276e46
Dmitry Alinsky <alinsky.dmitry@gmail.com>
2022-03-05 20:24:57 +0500

    Update InformationController.php

--- api/controllers/InformationController.php
+++ api/controllers/InformationController.php
@@ -10,13 +10,14 @@ use yii\helpers\ArrayHelper;
 use backend\modules\System\models\SystemSettings;
 use backend\modules\System\models\SystemLanguage;
 use backend\modules\Staticpage\models\Staticpage;
+use backend\modules\Translation\models\TranslationSource;
 
 /**
-* Here you can find information about:
-* - System settings
-* - Languages
-* - Static pages
-*/
+ * Here you can find information about:
+ * - System settings
+ * - Languages
+ * - Static pages
+ */
 class InformationController extends RestController
 {
     public function behaviors()
@@ -30,7 +31,7 @@ class InformationController extends RestController
             ],
         ]);
     }
-    
+
     public function actionIndex()
     {
         return [
@@ -38,5 +39,17 @@ class InformationController extends RestController
             'languages' => Yii::$app->services->i18n::$languages,
             'staticpages' => Staticpage::find()->select(['name', 'label'])->indexBy('name')->asObject()->all(),
         ];
+    }
+
+    public function actionTranslations()
+    {
+        $translations = TranslationSource::find()
+            ->with(['translations'])
+            ->andWhere(['category' => 'app_mobile'])
+            ->asArray()->all();
+
+        return ArrayHelper::map($translations, 'message', function ($value) {
+            return ArrayHelper::map($value['translations'], 'language', 'translation');
+        });
     }
 }
